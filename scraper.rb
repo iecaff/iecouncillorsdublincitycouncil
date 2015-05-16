d38c3e8b2c7ed125e537d66bf8d32d24be314b9e
iecaff <roscaf@gmail.com>
2015-05-16 20:08:16 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -32,7 +32,7 @@ page_urls.each do |con_link|
      con_area = con_page.at("strong:contains('Area')").next.next.text
      puts con_area
      end
-     if con_page.at("strong:contains('Phone')").next_element.next.text
+     if con_page.at("strong:contains('Phone')").parent.text
      con_phone = ""
      elsif con_page.at("strong:contains('Phone')").next.next.text
      con_phone = con_page.at("strong:contains('Phone')").next.next.text
