60389760376669f67734661cce8de99b5f440090
iecaff <roscaf@gmail.com>
2015-05-16 18:52:44 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -31,9 +31,7 @@ page_urls.each do |con_link|
      con_area = con_page.at("strong:contains('Area')").next.next.text
      puts con_area
      end
-     if con_page.at("strong:contains('Phone')").next.next.nil
-     con_phone = con_page.at("strong:contains('Phone')").next.text
-     else
+     if con_page.at("strong:contains('Phone')").next.next.text
      con_phone = con_page.at("strong:contains('Phone')").next.next.text
      puts con_phone
      end
