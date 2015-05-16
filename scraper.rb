7c33414df2f6aec519e5d6f8a2b547c5860448dc
iecaff <roscaf@gmail.com>
2015-05-16 19:45:42 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -23,6 +23,7 @@ page_urls.each do |con_link|
      end
      if con_page.at("strong:contains('Email')").next.nil?
      con_email = con_page.at("strong:contains('Email')").parent.next_element.at('a').next.text
+          puts con_email
      else     
      con_email = con_page.at("strong:contains('Email')").next.next.text
      puts con_email
