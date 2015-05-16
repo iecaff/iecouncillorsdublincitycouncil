e7e84dd83e6c2d173b02867bd9d245798079a101
iecaff <roscaf@gmail.com>
2015-05-16 13:22:13 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ page_urls = front_page.search('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_R
 
 page_urls.each do |con_link|
       con_page = Mechanize::Page::Link.new( con_link, mechanize, front_page ).click
-    puts con_page.at('div#eventDetails br').text 
+    puts con_page.at('div#eventDetails br') 
   
 end
 
