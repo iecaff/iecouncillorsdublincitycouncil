b51a9d786fe41a1c4c5285f28dc4d2800df26193
iecaff <roscaf@gmail.com>
2015-05-16 14:40:13 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,7 @@ page_urls = front_page.search('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_R
 page_urls.each do |con_link|
       con_page = Mechanize::Page::Link.new( con_link, mechanize, front_page ).click
        if con_page.at('picture img')
-         image_url = con_page.at('picture source')['srcset'].uri.query=nil
+         image_url = con_page.at('picture source')['srcset'].gsub(/\?.*/, '')
        else
         image_url = con_page.at('div#textContent img')['src']
   end
