6e7ea6ae7393bde764e4691368c03adaadc5431a
iecaff <roscaf@gmail.com>
2015-05-16 16:44:44 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -17,36 +17,12 @@ page_urls.each do |con_link|
      con_image = con_page.uri.merge image_url
      puts con_image
      
-     if con_page.at("strong:contains('Ward')").next.next.text
-     con_ward = con_page.at("strong:contains('Ward')").next.next.text
-     puts con_ward
-     end
-     if con_page.at("strong:contains('Email')").next.nil?
-     con_email = con_page.at("span:contains('mailto')").next.text
-     else     
-     con_email = con_page.at("strong:contains('Email')").next.next.text
-     puts con_email
-     end
-          if con_page.at("strong:contains('Area')").next.next.text
-     con_area = con_page.at("strong:contains('Area')").next.next.text
-     puts con_area
-     end
-     if con_page.at("strong:contains('Phone')").next.next.text
-     con_phone = con_page.at("strong:contains('Phone')").next.next.text
-     puts con_phone
-     end
-          if con_page.at("strong:contains('Mobile')").next.next.text
-     con_mobile = con_page.at("strong:contains('Mobile')").next.next.text
-     puts con_mobile
-     end
-          if con_page.at("strong:contains('Party')").next.next.text
-     con_party = con_page.at("strong:contains('Party')").next.next.text
-     puts con_party
-     end
-     if con_page.at("strong:contains('Address')").next.next.text
-     con_address = con_page.at("strong:contains('Address')").next.next.text
-     puts con_address
-     end
+    con_text = con_page.search('div#eventDetails p')
+    con_text.each do |con_string|
+    puts con_string
+    end
+ 
+ 
 end
 
 
