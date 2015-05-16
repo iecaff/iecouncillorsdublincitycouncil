c1c1b490030f7f6e30e99b7b5ff5ca8ab1643e50
iecaff <roscaf@gmail.com>
2015-05-16 13:14:19 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -4,11 +4,13 @@ require 'mechanize'
 
 mechanize = Mechanize.new
 
-page = mechanize.get('http://www.dublincity.ie/main-menu-your-council-your-city-councillors/full-councillor-list')
-page_urls = page.search('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_RichHtmlField a')
+front_page = mechanize.get('http://www.dublincity.ie/main-menu-your-council-your-city-councillors/full-councillor-list')
+page_urls = front_page.search('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_RichHtmlField a')
 
 page_urls.each do |con_link|
-  puts con_link.text
+      con_page = Mechanize::Page::Link.new( con_link, mechanize, front_page ).click
+    puts con_page.at('div#eventDetails').text 
+  
 end
 
 
