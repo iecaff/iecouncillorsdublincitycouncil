69ac1290b03db0c8b8071eb390ccc6ce54db8fe5
iecaff <roscaf@gmail.com>
2015-05-16 12:32:57 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,2 +1,9 @@
 require 'scraperwiki'
 require 'mechanize'
+
+
+mechanize = Mechanize.new
+
+page = mechanize.get('http://www.dublincity.ie/main-menu-your-council-your-city-councillors/full-councillor-list')
+
+puts page.at('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_RichHtmlField tr')
