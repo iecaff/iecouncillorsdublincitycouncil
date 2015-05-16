b5473ab2958e060fd47869d22d4051c705a8a5a0
iecaff <roscaf@gmail.com>
2015-05-16 18:08:37 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -8,8 +8,7 @@ front_page = mechanize.get('http://www.dublincity.ie/main-menu-your-council-your
 page_urls = front_page.search('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_RichHtmlField a')
 
 page_urls.each do |con_link|
-      con_url = Mechanize::Page::Link.new( con_link, mechanize, front_page )
-      con_page = con_url.click
+      con_page = Mechanize::Page::Link.new( con_link, mechanize, front_page ).click
        if con_page.at('picture img')
          image_url = con_page.at('picture source')['srcset'].gsub(/\s.+/, '')
        else
@@ -18,9 +17,40 @@ page_urls.each do |con_link|
      con_image = con_page.uri.merge image_url
      puts con_image
      
-    con_doc = con_page.parser
-    puts con_doc.css('div.eventDetails').text
- 
+     if con_page.at("strong:contains('Ward')").next.next.text
+     con_ward = con_page.at("strong:contains('Ward')").next.next.text
+     puts con_ward
+     end
+     if con_page.at("strong:contains('Email')").next.nil?
+     con_email = con_page.at("span:contains('mailto')").next.text
+     else     
+     con_email = con_page.at("strong:contains('Email')").next.next.text
+     puts con_email
+     end
+          if con_page.at("strong:contains('Area')").next.next.text
+     con_area = con_page.at("strong:contains('Area')").next.next.text
+     puts con_area
+     end
+     if con_page.at("strong:contains('Phone')").next.next.text
+     con_phone = con_page.at("strong:contains('Phone')").next.next.text
+     puts con_phone
+     end
+          if con_page.at("strong:contains('Mobile')").next.next.text
+     con_mobile = con_page.at("strong:contains('Mobile')").next.next.text
+     puts con_mobile
+     end
+          if con_page.at("strong:contains('Party')").next.next.text
+     con_party = con_page.at("strong:contains('Party')").next.next.text
+     puts con_party
+     end
+     if con_page.at("strong:contains('Address')").next.next.text
+     con_address = con_page.at("strong:contains('Address')").next.next.text
+     puts con_address
+     end
 end
 
+
+    Status API Training Shop Blog About 
+
+    © 2015 GitHub, Inc. Terms Privacy Security Contact 
 
