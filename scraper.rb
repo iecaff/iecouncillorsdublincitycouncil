3815d46727f8c1883397ed9e06dbc9525eadebd1
iecaff <roscaf@gmail.com>
2015-05-17 14:10:29 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -17,7 +17,7 @@ page_urls.each do |con_link|
      con_image = con_page.uri.merge image_url
      puts con_image
      
-     con_text = con_page.xpath('//text()').text
+     con_text = con_page.at('//text()').text
          
            puts con_text
 end
