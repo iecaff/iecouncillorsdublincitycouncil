42574757f506b7c89550f98bfd8c44a46b7229bb
iecaff <roscaf@gmail.com>
2015-05-17 19:27:03 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -17,7 +17,7 @@ page_urls.each do |con_link|
      con_image = con_page.uri.merge image_url
      puts con_image
      
-     con_text = con_page.parser.css('div#eventdetails')
+     con_text = con_page.parser.css('div[@id=eventDetails]')
         puts con_text
             
      
