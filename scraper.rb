a7a13733b8efa9b318ef824b77b9c6ac1a557c74
iecaff <roscaf@gmail.com>
2015-05-18 17:56:19 +0100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -16,6 +16,16 @@ con_record = {
 :address => ''
 }
 
+con_area = ''
+con_ward = ''
+con_name = ''
+con_party = ''
+con_email = ''
+con_phone = ''
+con_mobile = ''
+con_image = ''
+con_address = ''
+
 front_page = mechanize.get('http://www.dublincity.ie/main-menu-your-council-your-city-councillors/full-councillor-list')
 page_urls = front_page.search('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_RichHtmlField a')
 con_info = ['Area:','Ward:','Elected:','Party:','Address:','Email:','Phone:','Mobile:','Fax:',nil]
